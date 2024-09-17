1a78a4d1e633d199a6332ed3ebbfc26e5026a2a0
maustem <diego.maustem@gmail.com>
2024-09-17 17:22:12 -0300

    :test_tube: test: Type of variables go

---
+++ Tipos-Go/tipos.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Printf("Type: %T - Value: %v\n", "Usuario", "Usuario")
+	fmt.Printf("Type: %T - Value: %v\n", 1, 1)
+	fmt.Printf("Type: %T - Value: %v\n", 1.333, 1.333)
+	fmt.Printf("Type: %T - Value: %v\n", true, true)
+}
