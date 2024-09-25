0eb92015743f669609ebaa7521a73cb41e335841
maustem <diego.maustem@gmail.com>
2024-09-24 23:02:32 -0300

    :test_tube: test: Implementing ararys go

---
+++ listas-Go/arrays.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// First exemple of declaration array
+	var array [2]string
+	array[0] = "Hello"
+	array[1] = "World"
+	// fmt.Println(array)
+
+	// Seccond example of declaration array
+	numberPrimos := [6]int{1, 2, 3, 4, 5, 6}
+	fmt.Println(numberPrimos)
+	fmt.Println(numberPrimos[1:3])
+}
