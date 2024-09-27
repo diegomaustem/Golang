5782c5464cc1b76e9308ff9d7900db965e722ba8
maustem <diego.maustem@gmail.com>
2024-09-27 15:14:53 -0300

    :test_tube: test: Structs with type Person in go

--- Structs-Go/structs.go
+++ Structs-Go/structs.go
@@ -2,16 +2,29 @@ package main
 
 import "fmt"
 
-type Pessoa struct {
+type Person struct {
 	Name  string
 	Age   int
 	Email string
 }
 
+// Defined struct with type Person
+type Occupation struct {
+	Person
+	Type string
+}
+
 func main() {
 	// Assigning a struct to a variable
-	person := Pessoa{Name: "Matheus", Age: 21, Email: "matheus@gmail.com"}
+	person := Person{Name: "Matheus", Age: 21, Email: "matheus@gmail.com"}
 
 	fmt.Println("Hi, I am", person.Name)
 	fmt.Println("I am,", person.Age, "years old")
+
+	// Getting struct with type Person
+	occupation := Occupation{person, "Developer"}
+
+	fmt.Println("Hi, I am a", occupation.Type)
+	fmt.Println("Hi, my name is", occupation.Person.Name)
+	fmt.Println("My occupation is", occupation.Person.Age)
 }
